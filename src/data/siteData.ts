9b0a9a44c4d1538d12df73a71117733a053177af
Arturo Castro <coding.portafolio@outlook.es>
2025-12-11 06:09:08 +0200

    Update siteData.ts

--- src/data/siteData.ts
+++ src/data/siteData.ts
@@ -235,7 +235,7 @@ export const activities: Activity[] = [
   {
     id: 'finnish-sauna',
     duration: '2h',
-    image: 'https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?w=800',
+    image: '/finnish-sauna.jpg',
     translations: {
       fi: {
         name: 'Perinteinen suomalainen sauna',
